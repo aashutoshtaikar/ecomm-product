e68b154dbb4e73fe1764730857b23e0989a55001
ataikar <aashutoshtaikar@gmail.com>
2019-09-30 03:38:10 -0400

    removed unwanted files

--- src/test/java/com/ecommerce/product/ProductApplicationTests.java
+++ src/test/java/com/ecommerce/product/ProductApplicationTests.java
@@ -1,16 +1,16 @@
-//package com.ecommerce.product;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class ProductApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
+package com.ecommerce.product;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
